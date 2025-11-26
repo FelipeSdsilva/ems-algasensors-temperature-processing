a03a9e876ec1d5aa03b5aa4bd3c677d68af6ba9f
Felipe Sousa <felipe.fps09@hotmail.com>
2025-11-26 11:40:28 -0300

    Feat: Create tests and run configuration.

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/utils/uuid/UUIDv7Util.java
@@ -0,0 +1,22 @@
+package com.algaworks.algasensors.temperature.processing.utils.uuid;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.UUID;
+
+public class UUIDv7Util {
+
+  public UUIDv7Util() {
+  }
+
+  public static OffsetDateTime extractOffsetDateTime(UUID uuid) {
+    if (uuid == null)
+      return null;
+
+    @SuppressWarnings("squid:S1854")
+    long timestamp = uuid.getMostSignificantBits() >>> 16;
+
+    return OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+  }
+}
---
+++ src/test/java/com/algaworks/algasensors/temperature/processing/utils/UUIDv7Test.java
@@ -0,0 +1,24 @@
+package com.algaworks.algasensors.temperature.processing.utils;
+
+import com.algaworks.algasensors.temperature.processing.utils.uuid.IdGenerator;
+import com.algaworks.algasensors.temperature.processing.utils.uuid.UUIDv7Util;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+public class UUIDv7Test {
+
+  @Test
+  void shouldGenerateUUIDv7(){
+    UUID uuid = IdGenerator.generateTimeBasedUUID();
+
+    OffsetDateTime uuidDateTime = UUIDv7Util.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+    OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+    Assertions.assertThat(uuidDateTime).isEqualTo(currentDateTime);
+
+  }
+}
